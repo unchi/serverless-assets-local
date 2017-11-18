6525289f103901bf2b9988390f235d6ac6eb9dc9
unchi <developer.japan@gmail.com>
2017-11-19 01:19:16 +0900

    commend 修正

--- index.js
+++ index.js
@@ -13,37 +13,39 @@ class ServerlessAssetsLocal {
     this.client = null;
 
     this.commands = {
-      start: {
-        usage: 'Start Assets local server.',
-        lifecycleEvents: ['startHandler'],
-        options: {
-          port: {
-            shortcut: 'p',
-            usage: 'The port number that Assets will use to communicate with your application. If you do not specify this option, the default port is 8080',
+      assets: {
+        start: {
+          usage: 'Start Assets local server.',
+          lifecycleEvents: ['startHandler'],
+          options: {
+            port: {
+              shortcut: 'p',
+              usage: 'The port number that Assets will use to communicate with your application. If you do not specify this option, the default port is 8080',
+            },
+            originPath: {
+              shortcut: 'o',
+              usage: ''
+            },
+            directory: {
+              shortcut: 'd',
+              usage: 'The directory where Assets directory.',
+            },
+            noStart: {
+              shortcut: 'n',
+              default: false,
+              usage: 'Do not start Assets local (in case it is already running)',
+            },
+            cors: {
+              shortcut: 'c',
+              usage: 'Enable CORS',
+            },
+           // host: {},
+           // key: {},
+           // cert: {},
+           // pfx: {},
           },
-          originPath: {
-            shortcut: 'o',
-            usage: ''
-          },
-          directory: {
-            shortcut: 'd',
-            usage: 'The directory where Assets directory.',
-          },
-          noStart: {
-            shortcut: 'n',
-            default: false,
-            usage: 'Do not start Assets local (in case it is already running)',
-          },
-          cors: {
-            shortcut: 'c',
-            usage: 'Enable CORS',
-          },
-         // host: {},
-         // key: {},
-         // cert: {},
-         // pfx: {},
-        }
-      }
+        },
+      },
     };
     this.hooks = {
       'assets:start:startHandler': this.startHandler.bind(this),
